011413a3ce4fbf1d2e5d5b173bfaa5ef30c1fde3
shakhidzhanov-s <39909672+shakhidzhanov-s@users.noreply.github.com>
2018-08-06 22:21:28 +0300

    Update models.py

--- ru/models.py
+++ ru/models.py
@@ -240,7 +240,7 @@ class Attestation(models.Model):
 class Publication(models.Model):
     title = models.CharField(max_length=350)
     authors = models.CharField(max_length=500)
-    journal = models.CharField(max_length=50)
+    journal = models.CharField(max_length=100)
     volume = models.CharField(max_length=15)
     pages = models.CharField(max_length=20)
     date = models.DateField('date published')
